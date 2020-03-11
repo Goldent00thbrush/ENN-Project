3944d82dd71eb68dbc683e82e05633acb11d7ee5
Goldent00thbrush <44409654+Goldent00thbrush@users.noreply.github.com>
2020-03-11 15:42:44 +0200

    Add files via upload

--- Movecars.py
+++ Movecars.py
@@ -1,6 +1,8 @@
 import math
 import numpy
-
+import sys 
+import time
+import pdb
 from ApplyGA import ApplyGA
 from Feedforward import Feedforward
 
@@ -8,11 +10,37 @@ from Feedforward import Feedforward
 #  Prerequisites  Chromosomes,  Chromosomes_Fitness
 # Outputs  Fitness(standing vector: an element for each car)
 # Initializations
+def read_lidar(path):
+	try:
+		outfile = open(path, 'r')
+	except IOError:
+		read_lidar(path)
+	with outfile:
+		s = outfile.readlines()
+		outfile.close()	
+		data = []
+		for line in s:
+			reading = line.split('\t')
+			if (float(reading[2])!= 0.0):
+			 data.append(float(reading[2]))
+		return data
+def write_flag(path,value):
+	try:
+		outfile = open(path, 'r+')
+	except IOError:
+		read_lidar(path)
+	with outfile:
+		outfile.seek(0)
+		outfile.truncate(0)
+		outfile.write(str(value))
+		outfile.close()	
+		
+
 def MoveCars(env, nbrOfTimeStepsToTimeout, GA, dt, sensor, car, num, smallXYVariance, Chromosomes_Fitness, Chromosomes,
              Network_Arch, unipolarBipolarSelector, collison_value):
     carLocations = env.start_points  # Car Initial Location[X, Y] in [Meters]
     carHeadings = env.start_headings  # Car Initial Heading Counter Clock Wise[Degrees]
-    steerAngles = env.start_steerAngles  # [Degrees] Counter Clock Wise(Same for all cars)
+    steerAngles = 0 #env.start_steerAngles  # [Degrees] Counter Clock Wise(Same for all cars)
 
     # timesteps = 1
     # Old_Locations = []
@@ -47,10 +75,9 @@ def MoveCars(env, nbrOfTimeStepsToTimeout, GA, dt, sensor, car, num, smallXYVari
         LifeTimes = 0  # In number of draw steps(multiple of GA.dt)
         sensor_readings = []
         y = 0
-        print("Sensor readings: ")  ###############input sensor readings with angles - spectrum - distance
-        for i in range(sensor.size):
-            sensor_readings.append(int(input()))
-
+        sensor_readings = read_lidar(sys.argv[1])
+        while(sensor_readings == []):
+          sensor_readings = read_lidar(sys.argv[1])
         dist = min(sensor_readings)  #will be used to determines if there is a collision
         id = sensor_readings.index(dist)
 
@@ -167,6 +194,7 @@ def MoveCars(env, nbrOfTimeStepsToTimeout, GA, dt, sensor, car, num, smallXYVari
 
         outputs = Feedforward(sensor_readings, current_chromosome, Network_Arch, unipolarBipolarSelector)
         steerAngles = numpy.pi / 2 * (outputs[1] - outputs[0])  # From - 90 to 90 degrees
+
         frontWheel = []
         backWheel = []
         # 2D car steering physics(Calculate carLocation and carHeading)
@@ -181,7 +209,7 @@ def MoveCars(env, nbrOfTimeStepsToTimeout, GA, dt, sensor, car, num, smallXYVari
         for i in range(len(carLocations)):
             carLocations[i] = (frontWheel[i] + backWheel[i]) / 2
         carHeadings = math.atan2(frontWheel[1] - backWheel[1], frontWheel[0] - backWheel[0])
-
+        write_flag(sys.argv[4],steerAngles)
         # print("Front Wheel: ", frontWheel)
         # print("Back Wheel: ", backWheel)
         print("Steering Angles: ", steerAngles)
